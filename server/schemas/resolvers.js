0b89626f8b9e96ae3955a59059ad67819cbe7f1b
Cameron <cameron.wills89@outlook.com>
2020-12-31 12:59:30 -0500

    added queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { User, Thought } = require("../models");
+
+const resolvers = {
+  Query: {
+    //parent If we used nested resolvers to handle more complicated actions
+    thoughts: async (parent, { username }) => {
+      //the sort methord will return the thought data in descending order
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+  },
+};
+
+module.exports = resolvers;
